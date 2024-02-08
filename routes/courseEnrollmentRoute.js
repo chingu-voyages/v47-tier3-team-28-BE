834a39141af77794fe3fb09892e3399d258a914a
Ezz Maree <ezmarie97@gmail.com>
2024-02-08 23:36:41 +0200

    added authorization to enrollment routes

--- routes/courseEnrollmentRoute.js
+++ routes/courseEnrollmentRoute.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 const { courseEnroll, courseWithdrawl, listEnrolledCourses } = require('../controllers/enrollmentController');
+const { protect } = require('../middleware/authMiddleware');
 const router = Router();
 
-router.post('/student/:studentID/enroll/:courseID', courseEnroll);
-router.post('/student/:studentID/withdraw/:courseID', courseWithdrawl);
-router.get('/student/:studentID/courses', listEnrolledCourses);
+router.post('/student/:studentID/enroll/:courseID', protect, courseEnroll);
+router.post('/student/:studentID/withdraw/:courseID', protect, courseWithdrawl);
+router.get('/student/:studentID/courses', protect, listEnrolledCourses);
 
 
 module.exports = router;
